c80691b39af1788abb04ca86c28cab3613d64a80
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-10-05 18:43:23 -0300

    Preload the icon during instantiation

--- src/lock/actions.js
+++ src/lock/actions.js
@@ -3,8 +3,12 @@ import WebAPI from './web_api';
 import { getEntity, read, removeEntity, swap, setEntity, updateEntity } from '../store/index';
 import * as l from './index';
 import * as cs from '../cred/storage';
+import * as preload from '../preload/index';
 
 export function setupLock(id, clientID, domain, defaultOptions) {
+  const icon = defaultOptions && defaultOptions.icon;
+  preload.img((icon && typeof icon === "string") ? icon : l.DEFAULT_ICON_URL);
+
   const lock = l.setup({
     id: id,
     clientID: clientID,
--- src/lock/index.js
+++ src/lock/index.js
@@ -4,6 +4,8 @@ import { iconUrl } from '../icon/index';
 import * as d from '../dict/index';
 import t from '../dict/t';
 
+export const DEFAULT_ICON_URL = "//cdn.auth0.com/styleguide/1.0.0/img/badge.png";
+
 export function setup(attrs) {
   const { clientID, defaultOptions, domain, id } = attrs;
 
@@ -80,7 +82,7 @@ function extractUIOptions(id, options) {
     containerID: options.container || `auth0-lock-container-${id}`,
     appendContainer: !options.container,
     autoclose: undefined === options.autoclose ? false : closable && options.autoclose,
-    icon: options.icon || "//cdn.auth0.com/styleguide/1.0.0/img/badge.png",
+    icon: options.icon || DEFAULT_ICON_URL,
     closable: closable,
     dict: d.build(dictName, typeof options.dict === "object" ? options.dict : {}),
     focusInput: undefined === options.focusInput ? !(options.container || isSmallScreen()) : !!options.focusInput,
